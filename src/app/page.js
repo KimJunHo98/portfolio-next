33b4143b8afe91fc75ade4bd6495413146ef554a
KimJunHo98 <dydgnscc6845@gmail.com>
2023-10-09 19:27:40 +0900

    231009 로딩 끝난 후 화면 슬라이드업 효과 적용

--- src/app/page.js
+++ src/app/page.js
@@ -1,55 +1,68 @@
 "use client"; // effect를 사용하기 위해 ssr방식을 csr방식으로 변경
 
 import React, { useEffect, useState } from "react";
+import Loading from "../components/Loading";
 import Skip from "../components/Skip";
 import Header from "../components/Header";
 import Intro from "../components/Intro";
 import About from "../components/About";
 import Skill from "../components/Skill";
+import Site from "../components/Site";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
-import lenis from "../utils/lenis";
+
 import link from "../utils/link";
-import Site from "../components/Site";
-import Loading from "../components/Loading";
+import lenis from "../utils/lenis";
 
 const Home = () => {
     const [isLoading, setIsLoading] = useState(true);
+    const [isShow, setIsShow] = useState(false);
 
     useEffect(() => {
         setIsLoading(true);
+        setIsShow(false);
 
         const loadingTimer = setTimeout(() => {
             setIsLoading(false);
-        }, 3000);
+        }, 4000);
+        
+        const showTimer = setTimeout(() => {
+            setIsShow(true);
+        }, 4100);
 
-        return () => clearTimeout(loadingTimer);
+        return () => {
+            clearTimeout(loadingTimer);
+            clearTimeout(showTimer);
+        };
     }, []);
 
-    useEffect(() => {
-        lenis();
-        link();
-    }, []);
+    // useEffect(() => {
+    //     lenis();
+    //     link();
+    // }, []);
 
     return (
-        <div id="wrap">
+        <>
             {isLoading ? (
                 <Loading />
             ) : (
                 <>
-                    <Skip />
-                    <Header />
-                    <main id="main" role="main">
-                        <Intro />
-                        <About />
-                        <Skill />
-                        <Site />
-                        <Contact />
-                    </main>
-                    <Footer />
+                    <div id="slide_up" className={isShow ? "slide" : ""}></div>
+                    <div id="wrap">
+                        <Skip />
+                        <Header />
+                        <main id="main" role="main">
+                            <Intro />
+                            <About />
+                            <Skill />
+                            <Site />
+                            <Contact />
+                        </main>
+                        <Footer />
+                    </div>
                 </>
             )}
-        </div>
+        </>
     );
 };
 export default Home;
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,16 +1,15 @@
 import React from "react";
 
-const Loading = ({isLoading}) => {
+const Loading = () => {
     return (
-        <section id={`loading ${isLoading ? "" : "slide_up"}`}>
-            {/* isLoading prop활용해서 로딩 끝나면 slide_up 클래스 추가해서 로딩화면이 위로 슬라이드 되서 없어지는 효과 추가해야 하고 circle도 없어져야함  */}
-            <div className={`loading_box ${!isLoading ? "none" : ""}`}>
+        <div id="loading">
+            <div className="loading_box">
                 <div className="circle circle1"></div>
                 <div className="circle circle2"></div>
                 <div className="circle circle3"></div>
                 <div className="circle circle4"></div>
             </div>
-        </section>
+        </div>
     );
 };
 
