e90f34f89ce2f523ae21c10ea4c9191dce7ac100
KimJunHo98 <dydgnscc6845@gmail.com>
2023-11-29 18:21:31 +0900

    231129 아이폰 가로스크롤 문제 해결을 위해 css, 메타데이타 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,22 @@ import "../assets/scss/style.scss";
 export const metadata = {
     title: "KimJunHo's portfolio by Next",
     description: "Next.js를 사용한 개인 포트폴리오 사이트입니다.",
-    keywords: ["포트폴리오", "리액트", "넥스트", "김준호", "프론트엔드", "웹 퍼블리셔", "portfolio", "react.js", "next.js", "KimJunHo", "front-end", "web publisher"],
+    keywords: [
+        "포트폴리오",
+        "리액트",
+        "넥스트",
+        "김준호",
+        "프론트엔드",
+        "웹 퍼블리셔",
+        "portfolio",
+        "react.js",
+        "next.js",
+        "KimJunHo",
+        "front-end",
+        "web publisher",
+    ],
+    "apple-mobile-web-app-capable": "yes",
+    viewport: "minimum-scale=1.0, width=device-width, maximum-scale=1, user-scalable=no",
 };
 
 export default function RootLayout({ children }) {
