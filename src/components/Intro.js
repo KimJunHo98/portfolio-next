195c72e482bca4f27adff57dc746e88fbe820a7a
KimJunHo98 <dydgnscc6845@gmail.com>
2023-12-24 20:34:57 +0900

    231224 intro 컴포넌트 수정

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,44 +1,13 @@
 import React, { useEffect, useMemo, useRef } from "react";
-import { gsap } from "gsap";
 
 const Intro = () => {
-    const containerRef = useRef(null);
     const firstParaRef = useRef(null);
     const secondParaRef = useRef(null);
     const thirdParaRef = useRef(null);
-    const maskRef = useRef(null);
 
     const textArr1 = useMemo(() => "This is Junho Kim's portfolio site made with Next.js".split(" "), []);
-    const textArr2 = useMemo(() => "Scroll Down Scroll Down Scroll Down Scroll Down".split(" "), []);
-    const textArr3 = useMemo(() => "web publisher & frontend developer & web publisher & frontend developer &".split(" "), []);
-
-    // 텍스트 마우스 오버
-    useEffect(() => {
-        const container = containerRef.current;
-        const mask = maskRef.current;
-
-        const handleMouseMove = (e) => {
-            const y = window.innerHeight / 5;
-            const mouseY = e.clientY - y;
-
-            gsap.to(container, {
-                y: mouseY,
-                duration: 0.5,
-                ease: "power2.out",
-            });
-            gsap.to(mask, {
-                y: -mouseY,
-                duration: 0.5,
-                ease: "power2.out",
-            });
-        };
-
-        window.addEventListener("pointermove", handleMouseMove);
-
-        return () => {
-            window.removeEventListener("pointermove", handleMouseMove);
-        };
-    }, []);
+    const textArr2 = useMemo(() => "web publisher & frontend developer & web publisher & frontend developer &".split(" "), []);
+    const textArr3 = useMemo(() => "Scroll Down Scroll Down Scroll Down Scroll Down".split(" "), []);
 
     // 무한 마퀴텍스트
     useEffect(() => {
@@ -62,6 +31,7 @@ const Intro = () => {
         initTexts(secondPara, textArr2);
         initTexts(thirdPara, textArr3);
 
+        // 애니메이트 방향
         const marqueeText = (count, el, direction) => {
             if (count > el.scrollWidth / 2) {
                 el.style.transform = `translate3d(0, 0, 0)`;
@@ -73,6 +43,7 @@ const Intro = () => {
             return count;
         };
 
+        // 애니메이트 실행 함수
         const animate = () => {
             count1++;
             count2++;
@@ -94,31 +65,23 @@ const Intro = () => {
             <div className="container">
                 <div className="inner">
                     <div className="intro">
+                        <div className="parallel_container">
+                            <div className="cover first_cover">
+                                <p className="first_parallel parallel" ref={firstParaRef}></p>
+                            </div>
+                            <div className="cover second_cover">
+                                <p className="second_parallel parallel" ref={secondParaRef}></p>
+                            </div>
+                        </div>
                         <ul className="intro_text">
                             <li className="text_items">wellcome to my portfolio</li>
                             <li className="text_items">i&apos;d appreciate it if you</li>
                             <li className="text_items">watched it until the end</li>
                             <li className="text_items">please enjoy watch it</li>
-                            <li className="text_items">scroll down</li>
                         </ul>
-                        <div className="mask_container" ref={containerRef}>
-                            <ul className="intro_text_mask" ref={maskRef}>
-                                <li className="text_mask_items item1">wellcome to my portfolio</li>
-                                <li className="text_mask_items item3">i&apos;d appreciate it if you</li>
-                                <li className="text_mask_items item4">watched it until the end</li>
-                                <li className="text_mask_items item5">please enjoy watch it</li>
-                                <li className="text_mask_items item6">scroll down</li>
-                            </ul>
-                        </div>
                         <div className="parallel_container">
-                            <div className="cover">
-                                <p className="first-parallel parallel" ref={firstParaRef}></p>
-                            </div>
-                            <div className="cover">
-                                <p className="second-parallel parallel" ref={secondParaRef}></p>
-                            </div>
-                            <div className="cover">
-                                <p className="third-parallel parallel" ref={thirdParaRef}></p>
+                            <div className="cover third_cover">
+                                <p className="third_parallel parallel" ref={thirdParaRef}></p>
                             </div>
                         </div>
                     </div>
