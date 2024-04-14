7a2bf8ffe2dbaf2cd8f2d78c62551ff8265c278b
KimJunHo98 <dydgnscc6845@gmail.com>
2024-04-14 19:31:57 +0900

    240414 skill 컴포넌트 스코롤 값 수정

--- src/components/About1.js
+++ src/components/About1.js
@@ -48,13 +48,13 @@ const About1 = () => {
                                     안녕하세요, 신입 웹 퍼블리셔 김준호입니다.
                                 </p>
                                 <p className="text_items" ref={(el) => (textRef.current[3] = el)}>
-                                    퍼블리싱 & 개발 공부를 하며 전에는 느껴보지 못했던 재미와 오기를
+                                    퍼블리싱 & 프론트엔드 개발 공부를 하며 전에는 느껴보지 못했던 재미와 오기를
                                 </p>
                                 <p className="text_items" ref={(el) => (textRef.current[4] = el)}>
                                     느끼게 되며 웹 퍼블리셔 & 프론트엔드 개발자에 빠져들게 되었습니다.
                                 </p>
                                 <p className="text_items" ref={(el) => (textRef.current[5] = el)}>
-                                    아직은 실수도 많고 어렵지만 문제를 해결할 수 있는 능력을 갖추기 위해 지속적인 학습을 하고있습니다.
+                                    아직은 실수도 많고 어렵지만 문제를 해결할 수 있는 능력을 갖추기 위해 지속적인 학습을 하고 있습니다.
                                 </p>
                                 <p className="text_items" ref={(el) => (textRef.current[6] = el)}>
                                     사소한 어려움이라도 그것을 극복했을 때의 성취감이 배움을 멈출 수 없게 하는 것 같습니다.
