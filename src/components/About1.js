a7c9e21c51e31345f8015406f3096bb391606dcb
KimJunHo98 <dydgnscc6845@gmail.com>
2024-05-10 18:46:36 +0900

    240510 about 컴포넌트 반응형 적용, site 컴포넌트 패딩 값 수정

--- src/components/About1.js
+++ src/components/About1.js
@@ -36,15 +36,17 @@ const About1 = () => {
                 <div className="inner">
                     <div className="about">
                         <h2 className="about_title" ref={(el) => (textRef.current[0] = el)}>
-                            <span className="white500 bold">사용자 중심 웹 경험</span>을 위해 고민하는 <span className="bold">김준호</span>입니다
+                            <p className="block"><span className="white300 bold">사용자 중심 웹 경험</span>을 위해</p> 고민하는
+                            <span className="white200 bold">김준호</span>입니다
                         </h2>
                         <ul className="about_items">
                             <li className="item_list" ref={(el) => (textRef.current[2] = el)}>
-                                HTML5,CSS3, React, JavaScript(ES6) 기반의 웹 퍼블리셔 & 프론트엔드 개발자 김준호입니다.<br />
-                                인터랙션에 관심이 많으며, 웹 표준/접근성을 준수하여 최적의 사용자 경험을 제공해주는 것을 고민합니다.
+                                <p className="profile_text">HTML5, CSS3, React, JavaScript(ES6) 기반의 웹 퍼블리셔 & 프론트엔드 개발자 김준호입니다.</p>
+                                <p className="profile_text">인터랙션에 관심이 많으며, 웹 표준/접근성을 준수하여 최적의 사용자 경험을 제공해주는 것을 고민합니다.</p>
+                                <p className="profile_text">성장욕구를 바탕으로 지속적인 학습과 경험을 하여 성장하는 사람이 되겠습니다.</p>
                             </li>
-                            <li className="item_list" ref={(el) => (textRef.current[1] = el)}>
-                                <div className="profile_image_wrap">
+                            <li className="item_list profile_image_wrap" ref={(el) => (textRef.current[1] = el)}>
+                                <div className="profile_image_box">
                                     <Image
                                         src="/images/profile_image.png"
                                         alt="프로필 이미지"
